bb8295c6cdde132ed68f2278cbdbfe1ffc960675
Shromona Ghosh <shromona.ghosh@berkeley.edu>
2019-03-08 11:12:58 -0800

    Update cones_lanechange_falsifier.py

--- examples/webots/controllers/cones_lanechange_supervisor/cones_lanechange_falsifier.py
+++ examples/webots/controllers/cones_lanechange_supervisor/cones_lanechange_falsifier.py
@@ -30,17 +30,14 @@ BUFSIZE = 4096
 specification = ["G(collisioncone0 & collisioncone1 & collisioncone2)"]
 
 falsifier_params = DotMap()
-falsifier_params.port = PORT
 falsifier_params.n_iters = MAX_ITERS
-falsifier_params.maxreqs = MAXREQS
-falsifier_params.bufsize = BUFSIZE
 falsifier_params.compute_error_table = True
-sampler_params = DotMap()
-sampler_params.thres = 0.0
-falsifier_params.sampler_params = sampler_params
+
+server_options = DotMap(port=PORT, bufsize=BUFSIZE, maxreqs=MAXREQS)
 
 falsifier = mtl_falsifier(sample_space=sample_space, sampler_type=SAMPLERTYPE,
-                             specification=specification, falsifier_params=falsifier_params)
+                          specification=specification, falsifier_params=falsifier_params,
+                          server_options=server_options)
 falsifier.run_falsifier()
 
 print("Unsafe samples: Error table")
@@ -48,5 +45,3 @@ print(falsifier.error_table.table)
 
 # To save all samples: uncomment this
 # pickle.dump(falsifier.samples, open("generated_samples.pickle", "wb"))
-
-
