c7441ade59385277d9b645e979364f54940ab7aa
kesav-v <kesav@viswanadha.net>
2021-07-23 00:15:17 -0700

    Add script to run remaining experiments

---
+++ experiments/run_remaining.sh
@@ -0,0 +1,9 @@
+python experiments.py --path ieee/test_3.scenic --sampler-type mab
+python experiments.py --path ieee/intersection_01.scenic --sampler-type halton
+python experiments.py --path ieee/intersection_01.scenic --sampler-type mab
+python experiments.py --path ieee/intersection_03.scenic --sampler-type halton
+python experiments.py --path ieee/intersection_03.scenic --sampler-type mab
+python experiments.py --path ieee/ped_3.scenic --sampler-type halton
+python experiments.py --path ieee/ped_3.scenic --sampler-type mab
+python experiments.py --path ieee/composition.scenic --sampler-type halton --scenario Main
+python experiments.py --path ieee/composition.scenic --sampler-type mab --scenario Main
