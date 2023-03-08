1ef5b85d951ee5117d58b4e7abe17dddd349a316
nickovic <dejan.nickovic@ait.ac.at>
2023-03-08 19:15:57 +0100

    GLIS integration: algorithms + test file

--- tests/test_glisOptimization.py
+++ tests/test_glisOptimization.py
@@ -5,9 +5,8 @@ from dotmap import DotMap
 import pytest
 from tests.utils import sampleWithFeedback, checkSaveRestore
 
-pytest.importorskip('GPyOpt')
 
-def test_bayesianOptimization():
+def test_glisOptimization():
     carDomain = Struct({
         'position': Box([-10,10], [-10,10], [0,1]),
         'heading': Box([0, math.pi]),
@@ -21,10 +20,7 @@ def test_bayesianOptimization():
         sample = sample.cars[0].heading[0]
         return abs(sample - 0.75)
 
-    bo_params = DotMap()
-    bo_params.init_num = 2
-
-    sampler = FeatureSampler.bayesianOptimizationSamplerFor(space, BO_params=bo_params)
+    sampler = FeatureSampler.glisSamplerFor(space, n_initial_random=10)
 
     sampleWithFeedback(sampler, 3, f)
 
@@ -33,40 +29,8 @@ def test_save_restore(tmpdir):
         'a': Feature(DiscreteBox([0, 12])),
         'b': Feature(Box((0, 1)), lengthDomain=DiscreteBox((1, 2)))
     })
-    bo_params = DotMap(init_num=2)
-    sampler = FeatureSampler.bayesianOptimizationSamplerFor(space, bo_params)
+    sampler = FeatureSampler.glisSamplerFor(space, n_initial_random=3)
 
     checkSaveRestore(sampler, tmpdir)
-
-def test_BO_oper():
-    vals = []
-    N = 10
-    for k in range(N):
-        print("Testing k: ", k)
-        def f(sample):
-            x = sample.x[0]
-            return (6 * x - 2) ** 2 * np.sin(12 * x - 4)
-
-        space = FeatureSpace({'x': Feature(Box([0,1]))})
-
-        bo_params = DotMap()
-        bo_params.init_num = 5
-
-        sampler = FeatureSampler.bayesianOptimizationSamplerFor(space, BO_params=bo_params)
-
-        samples = []
-        y_samples = []
-
-        feedback = None
-        for i in range(20):
-            sample = sampler.nextSample(feedback)
-            samples.append(sample)
-            feedback = f(sample)
-            y_samples.append(feedback)
-
-        min_i = np.array(y_samples).argmin()
-        vals.append(np.linalg.norm(samples[min_i].x[0]-0.75) < 0.1)
-
-    assert sum(vals)/N >= 0.9
 
 
