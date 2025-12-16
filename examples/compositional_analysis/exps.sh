3493635013ed38dd21b8ab26078449584084889e
beyazit-y <beyazit.yalcinkaya@gmail.com>
2025-12-15 22:32:03 -0800

    Lower error bound in exps

--- examples/compositional_analysis/exps.sh
+++ examples/compositional_analysis/exps.sh
@@ -1,14 +1,14 @@
 # Fixed Error (epsilon = 0.1)
 ## Monolithic
-{ time python run_exp.py --expert --save_dir storage/fixed_error_monolithic_SX --confidence_level 0.95 --error_bound 0.1 --scenario "SX"; } &> storage/results/fixed_error_monolithic_SX.txt &
-{ time python run_exp.py --expert --save_dir storage/fixed_error_monolithic_COS --confidence_level 0.95 --error_bound 0.1 --scenario "COS"; } &> storage/results/fixed_error_monolithic_COS.txt &
-{ time python run_exp.py --expert --save_dir storage/fixed_error_monolithic_XSOC --confidence_level 0.95 --error_bound 0.1 --scenario "XSOC"; } &> storage/results/fixed_error_monolithic_XSOC.txt &
-{ time python run_exp.py --expert --save_dir storage/fixed_error_monolithic_SOCXS --confidence_level 0.95 --error_bound 0.1 --scenario "SOCXS"; } &> storage/results/fixed_error_monolithic_SOCXS.txt &
+{ time python run_exp.py --expert --save_dir storage/fixed_error_monolithic_SX --confidence_level 0.95 --error_bound 0.04295 --scenario "SX"; } &> storage/results/fixed_error_monolithic_SX.txt &
+{ time python run_exp.py --expert --save_dir storage/fixed_error_monolithic_COS --confidence_level 0.95 --error_bound 0.04295 --scenario "COS"; } &> storage/results/fixed_error_monolithic_COS.txt &
+{ time python run_exp.py --expert --save_dir storage/fixed_error_monolithic_XSOC --confidence_level 0.95 --error_bound 0.04295 --scenario "XSOC"; } &> storage/results/fixed_error_monolithic_XSOC.txt &
+{ time python run_exp.py --expert --save_dir storage/fixed_error_monolithic_SOCXS --confidence_level 0.95 --error_bound 0.04295 --scenario "SOCXS"; } &> storage/results/fixed_error_monolithic_SOCXS.txt &
 ## Compositional
-{ time python run_exp.py --expert --save_dir storage/fixed_error_compositional_SX --confidence_level 0.95 --error_bound 0.1 --scenario "SX" --compositional; } &> storage/results/fixed_error_compositional_SX.txt &
-{ time python run_exp.py --expert --save_dir storage/fixed_error_compositional_COS --confidence_level 0.95 --error_bound 0.1 --scenario "COS" --compositional; } &> storage/results/fixed_error_compositional_COS.txt &
-{ time python run_exp.py --expert --save_dir storage/fixed_error_compositional_XSOC --confidence_level 0.95 --error_bound 0.1 --scenario "XSOC" --compositional; } &> storage/results/fixed_error_compositional_XSOC.txt &
-{ time python run_exp.py --expert --save_dir storage/fixed_error_compositional_SOCXS --confidence_level 0.95 --error_bound 0.1 --scenario "SOCXS" --compositional; } &> storage/results/fixed_error_compositional_SOCXS.txt &
+{ time python run_exp.py --expert --save_dir storage/fixed_error_compositional_SX --confidence_level 0.95 --error_bound 0.04295 --scenario "SX" --compositional; } &> storage/results/fixed_error_compositional_SX.txt &
+{ time python run_exp.py --expert --save_dir storage/fixed_error_compositional_COS --confidence_level 0.95 --error_bound 0.04295 --scenario "COS" --compositional; } &> storage/results/fixed_error_compositional_COS.txt &
+{ time python run_exp.py --expert --save_dir storage/fixed_error_compositional_XSOC --confidence_level 0.95 --error_bound 0.04295 --scenario "XSOC" --compositional; } &> storage/results/fixed_error_compositional_XSOC.txt &
+{ time python run_exp.py --expert --save_dir storage/fixed_error_compositional_SOCXS --confidence_level 0.95 --error_bound 0.04295 --scenario "SOCXS" --compositional; } &> storage/results/fixed_error_compositional_SOCXS.txt &
 
 # Fixed Time Budget (2 mins)
 ## Monolithic
